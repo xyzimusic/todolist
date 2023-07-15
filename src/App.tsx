4eade0e0ef492979b13c697f1ca709ad2cf43612
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-07-15 12:21:27 +0300

    micro-tasks-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,18 @@
-import React, {useState} from 'react';
 import './App.css';
-import {Country} from "./Country";
-
-export type BanknotsType = 'Dollars' | 'RUBLS' | 'All'
-export type MoneyType = {
-    banknotes: BanknotsType
-    value: number// не ленимся, убираем заглушку, и пишем правильный тип)
-    number: string// ложку за Димыча, за...
-}
-
-let defaultMoney: MoneyType[] = [  // типизируем
-    {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
-    {banknotes: 'RUBLS', value: 100, number: ' w1234567890'},
-    {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
-    {banknotes: 'RUBLS', value: 100, number: ' r1234567890'},
-    {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
-    {banknotes: 'RUBLS', value: 50, number: ' v1234567890'},
-]
-
-// типизируем на входе и выходе
-export const moneyFilter = (money: MoneyType[], filter: BanknotsType): MoneyType[] => {
-    if (filter === 'All') return money
-    return money.filter((el: MoneyType) => el.banknotes === filter)
-    //если пришел filter со значением 'All', то возвращаем все банкноты
-    //return money.filter... ну да, придется фильтровать
-}
+import { HW1 } from './HW/1/HW1';
+import { HW2 } from './HW/2/HW2';
+import { HW3 } from './HW/3/HW3';
+import { HW4 } from './HW/4/HW4';
 
 function App() {
-
-    const [money, setMoney] = useState<MoneyType[]>(defaultMoney)
-    const [filterValue, setFilterValue] = useState<any>('All')   // по умолчанию указываем все банкноты
-
-    // а вот сейчас притормаживаем. И вдумчиво: константа filteredMoney получает результат функции moneyFilter
-    // в функцию передаем деньги и фильтр, по которому ихбудем выдавать(ретёрнуть)
-
-    const filteredMoney = moneyFilter(money, filterValue)
-    console.log(filteredMoney)
-    return (
-        <div className="App">
-            <Country
-                data={filteredMoney}   //отрисовать будем деньги после фильтрации
-                setFilterValue={setFilterValue}  //useState передаем? Так можно было?!
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      {/*<HW1 />*/}
+      {/*<HW2 />*/}
+      <HW3 />
+      {/*<HW4 />*/}
+    </div>
+  );
 }
-
-// Итого: в этой компоненте у нас мозги. А вот отрисовка где-то глубже. Погружаемся в Country
-
 
 export default App;
