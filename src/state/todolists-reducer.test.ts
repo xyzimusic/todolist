0386bd750f2fc887c0c58239b09468ae9823da21
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-08-11 19:56:00 +0300

    todo-9

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -16,7 +16,6 @@ test('correct todolist should be removed', () => {
     const startState: TodolistType[] = [
         {id: todolistId1, title: 'What to learn', filter: 'all'},
         {id: todolistId2, title: 'What to buy', filter: 'all'}
-
     ]
 
     const endState = todolistsReducer(startState, RemoveTodolistAC(todolistId1))
